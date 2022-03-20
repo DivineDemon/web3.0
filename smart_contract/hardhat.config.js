0e15393edd0d58316942b523b742317769a7ee78
DivineDemon <supame123@gmail.com>
2022-03-21 03:26:51 +0400

    Smart Contract Complete :sparkles: :rocket:

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,21 +1,13 @@
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.13",
+  networks: {
+    ropsten: {
+      url: "https://eth-ropsten.alchemyapi.io/v2/Q4uEac3drJpruG3BkPmCXh5eOU-r-dSP",
+      accounts: [
+        "577d9ac7999ffc5460849fa731e866aed1a90c18949f1933272c01656c67697e",
+      ],
+    },
+  },
 };
